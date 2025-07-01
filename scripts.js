ba3621f3e45c369522f719d3b5bcd1a0138bb953
shirl3yyyy <shirleycaren254@gmail.com>
2025-07-01 10:23:26 +0300

    second commit

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Your code goes here
+  console.log("DOM fully loaded and parsed");
+
+  const box = document.getElementById("color-box")
+  const btn = document.getElementById("change-color-btn")
